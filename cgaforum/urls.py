a329d2b9adbb1506b0dc1d639ee65b543bd045c8
reSetRoot <wei860925@gmail.com>
2022-01-11 02:31:47 +0800

    Modified: 	subcategory list layout. 	category list layout.
    Added:
    	topic detail page.

--- cgaforum/urls.py
+++ cgaforum/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 
-from cgaforum.views import CategoriesView, SubCategoriesView, TopicsView
+from cgaforum.views import CategoriesView, SubCategoriesView, TopicsView, TopicView
 
 
 urlpatterns = [
@@ -10,6 +10,9 @@ urlpatterns = [
         path('<slug:category_slug>', SubCategoriesView.as_view(), name='subcategories'),
         path('<slug:category_slug>/', include([
             path('<slug:subcategory_slug>', TopicsView.as_view(), name='topics'),
+            path('<slug:subcategory_slug>/', include([
+                path('<pk>', TopicView.as_view(), name='topic'),
+            ])),
         ])),
     ]))
 ]
--- cgaforum/views.py
+++ cgaforum/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.db.models import Q
 
 from cgaforum.models import Category, SubCategory, Topic
@@ -42,4 +42,17 @@ class TopicsView(ListView):
         subcategory_criteria = Q(f=category) & Q(slug=self.kwargs.get('subcategory_slug'))
         subcategory = get_object_or_404(SubCategory, subcategory_criteria)
         topic_criteria = Q(f=subcategory) & (Q(status=TopicStatus.Published.value[0]) | Q(status=TopicStatus.Privacy.value[0]))
-        return Topic.objects.filter(topic_criteria)
+        return Topic.objects.filter(topic_criteria).order_by('-published_time')
+
+
+class TopicView(DetailView):
+    model = Topic
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TopicView, self).get_context_data(object_list=None, **kwargs)
+        category = get_object_or_404(Category, slug=self.kwargs.get('category_slug'))
+        criteria = Q(f=category) & Q(slug=self.kwargs.get('subcategory_slug'))
+        subcategory = get_object_or_404(SubCategory, criteria)
+        context['category'] = category
+        context['subcategory'] = subcategory
+        return context
