7a4414062c287ec9c074d54f84f486043ba2dd81
reSetRoot <wei860925@gmail.com>
2022-01-19 20:33:00 +0800

    Added: 	Case detail page. 	Cases in case section list page.

--- cga_case/urls.py
+++ cga_case/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
 
 
-from cga_case.views import CaseCategoriesView, CaseSectionsView
+from cga_case.views import CaseCategoriesView, CaseSectionsView, CaseDetailView
 
 urlpatterns = [
     path('cgacase', CaseCategoriesView.as_view(), name='cgacase'),
-    path('<str:category_name>', CaseSectionsView.as_view(), name='case_sections'),
+    path('cgacase/', include([
+        path('<str:category_name>', CaseSectionsView.as_view(), name='case_sections'),
+        path('detail/', include([
+            path('<str:case_title>', CaseDetailView.as_view(), name='case_detail')
+        ])),
+    ])),
 ]
--- cga_case/views.py
+++ cga_case/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 from cga_case.models import CaseCategory, CaseSection, Case
@@ -21,3 +21,16 @@ class CaseSectionsView(ListView):
     def get_queryset(self):
         category = get_object_or_404(CaseCategory, name=self.kwargs.get('category_name'))
         return CaseSection.objects.filter(is_part_of=category)
+
+
+class CaseDetailView(DetailView):
+    model = Case
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CaseDetailView, self).get_context_data(object_list=None, **kwargs)
+        case = get_object_or_404(Case, title=self.kwargs.get('case_title'))
+        context['category'] = case.is_one_of.is_part_of
+        return context
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Case, title=self.kwargs.get('case_title'))
